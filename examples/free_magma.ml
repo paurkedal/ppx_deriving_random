c6b5a1179fb2b91e00d603c54884c95cdffdd81c
Petter Urkedal <paurkedal@gmail.com>
2015-03-29 20:15:59 +0200

    Add complete example from the readme.

---
+++ examples/free_magma.ml
@@ -0,0 +1,20 @@
+(* Preamble. *)
+type random_state = Random.State.t
+let random_case i rng = Random.State.int rng i
+let random_case_30b = Random.State.bits
+
+(* Specialized string as variable. *)
+type var = string [@@deriving show]
+let random_var rng = String.make 1 (Char.chr (Random.State.int rng 26 + 0x61))
+
+(* Type and random generator. *)
+type 'a free_magma =
+  | Fm_gen [@weight 3] of 'a
+  | Fm_mul [@weight 2] of 'a free_magma * 'a free_magma
+  [@@deriving random, show]
+
+(* Test. *)
+let () =
+  let rng = Random.State.make_self_init () in
+  let m = random_free_magma random_var rng in
+  print_endline (show_free_magma pp_var m)
