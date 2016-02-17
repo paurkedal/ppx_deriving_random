171b1055318ba6d3f6f695a0968c3f9b641058a3
Petter A. Urkedal <paurkedal@gmail.com>
2016-02-17 19:32:39 +0100

    Expand tabs.

--- ppx_deriving_random.ml
+++ ppx_deriving_random.ml
@@ -1,4 +1,4 @@
-(* Copyright (C) 2015  Petter Urkedal <paurkedal@gmail.com>
+(* Copyright (C) 2015--2016  Petter A. Urkedal <paurkedal@gmail.com>
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by
@@ -56,7 +56,7 @@ let parse_options = List.iter @@ fun (name, pexp) ->
   match name with
   | _ ->
     raise_errorf ~loc:pexp.pexp_loc
-		 "The %s deriver takes no option %s." deriver name
+                 "The %s deriver takes no option %s." deriver name
 
 let tuple_opt = function
   | [] -> None
@@ -68,7 +68,7 @@ let mapped_expr e =
 
 let get_random_fun attrs =
   attrs |> Ppx_deriving.attr ~deriver "random"
-	|> Ppx_deriving.Arg.(get_attr ~deriver mapped_expr)
+        |> Ppx_deriving.Arg.(get_attr ~deriver mapped_expr)
 
 let get_weight attrs =
   let conv = function
@@ -76,7 +76,7 @@ let get_weight attrs =
     | {pexp_desc = Pexp_constant (Const_float x)} -> `Ok (`Float x)
     | _ -> `Error "@weight must be a constant int or float." in
   attrs |> Ppx_deriving.attr ~deriver "weight"
-	|> Ppx_deriving.Arg.(get_attr ~deriver conv)
+        |> Ppx_deriving.Arg.(get_attr ~deriver conv)
 
 let get_weight_float attrs =
   match get_weight attrs with
@@ -124,9 +124,9 @@ let cumulative get_attrs cs =
     (* On 32 bit platforms, 1.0 ± ε will be mapped to min_int. *)
     if x < 0 then max_int else x in
   let cs = cs |> List.map (fun (w, pcd) -> (normalize w, pcd))
-	      |> List.sort (fun x y -> compare (fst y) (fst x)) in
+              |> List.sort (fun x y -> compare (fst y) (fst x)) in
   fst @@ List.fold (fun (w, pcd) (acc, rem) -> (rem - w, pcd) :: acc, rem - w)
-		   cs ([], 1 lsl 30)
+                   cs ([], 1 lsl 30)
 
 let invalid_case =
   Exp.case [%pat? i]
@@ -138,11 +138,11 @@ let rec expr_of_typ typ =
     | Rtag (label, _, true, []) -> Exp.variant label None
     | Rtag (label, _, false, typs) ->
       Exp.variant label
-	(tuple_opt (List.map (fun typ -> [%expr [%e expr_of_typ typ]]) typs))
+        (tuple_opt (List.map (fun typ -> [%expr [%e expr_of_typ typ]]) typs))
     | Rinherit typ -> expr_of_typ typ
     | _ ->
       raise_errorf ~loc:typ.ptyp_loc "Cannot derive %s for %s."
-		   deriver (Ppx_deriving.string_of_core_type typ) in
+                   deriver (Ppx_deriving.string_of_core_type typ) in
   match get_random_fun typ.ptyp_attributes with
   | Some f -> app f [[%expr rng]]
   | None ->
@@ -150,63 +150,63 @@ let rec expr_of_typ typ =
     | [%type: unit] -> [%expr ()]
     | {ptyp_desc = Ptyp_constr ({txt = lid}, typs)} ->
       let f =
-	Exp.ident (mknoloc (Ppx_deriving.mangle_lid (`Prefix "random") lid)) in
+        Exp.ident (mknoloc (Ppx_deriving.mangle_lid (`Prefix "random") lid)) in
       let args =
-	List.map (fun typ -> [%expr fun rng -> [%e expr_of_typ typ]]) typs in
+        List.map (fun typ -> [%expr fun rng -> [%e expr_of_typ typ]]) typs in
       app f (args @ [[%expr rng]])
     | {ptyp_desc = Ptyp_tuple typs} -> Exp.tuple (List.map expr_of_typ typs)
     | {ptyp_desc = Ptyp_variant (fields, _, _); ptyp_loc}
-	  when List.for_all (weight_is_one *< rowfield_attributes) fields ->
+          when List.for_all (weight_is_one *< rowfield_attributes) fields ->
       let cases =
-	fields |> List.mapi @@ fun j field ->
-	  let result = expr_of_rowfield field in
-	  Exp.case (Pat.constant (Const_int j)) result in
+        fields |> List.mapi @@ fun j field ->
+          let result = expr_of_rowfield field in
+          Exp.case (Pat.constant (Const_int j)) result in
       Exp.match_
-	[%expr random_case [%e Exp.constant (Const_int (List.length cases))] rng]
-	(cases @ [invalid_case])
+        [%expr random_case [%e Exp.constant (Const_int (List.length cases))] rng]
+        (cases @ [invalid_case])
     | {ptyp_desc = Ptyp_variant (fields, _, _); ptyp_loc} ->
       let branch (w, field) cont =
-	[%expr if w > [%e Exp.constant (Const_int w)]
-	       then [%e expr_of_rowfield field]
-	       else [%e cont] ] in
+        [%expr if w > [%e Exp.constant (Const_int w)]
+               then [%e expr_of_rowfield field]
+               else [%e cont] ] in
       begin match cumulative rowfield_attributes fields with
       | [] -> assert false
       | (w, field) :: fields ->
-	[%expr let w = random_case_30b rng in
-	       [%e List.fold branch fields (expr_of_rowfield field)]]
+        [%expr let w = random_case_30b rng in
+               [%e List.fold branch fields (expr_of_rowfield field)]]
       end
     | {ptyp_desc = Ptyp_var name} -> [%expr [%e evar ("poly_" ^ name)] rng]
     | {ptyp_desc = Ptyp_alias (typ, _)} -> expr_of_typ typ
     | {ptyp_loc} ->
       raise_errorf ~loc:ptyp_loc "Cannot derive %s for %s."
-		   deriver (Ppx_deriving.string_of_core_type typ)
+                   deriver (Ppx_deriving.string_of_core_type typ)
 
 let expr_of_type_decl ({ptype_loc = loc} as type_decl) =
   let expr_of_constr pcd =
     Exp.construct {txt = Lident pcd.pcd_name.txt; loc = pcd.pcd_name.loc}
-		  (tuple_opt (List.map expr_of_typ pcd.pcd_args)) in
+                  (tuple_opt (List.map expr_of_typ pcd.pcd_args)) in
   match type_decl.ptype_kind, type_decl.ptype_manifest with
   | Ptype_abstract, Some manifest ->
     [%expr fun rng -> [%e expr_of_typ manifest]]
   | Ptype_variant constrs, _
-	when List.for_all (weight_is_one *< pcd_attributes) constrs ->
+        when List.for_all (weight_is_one *< pcd_attributes) constrs ->
     let make_case j pcd =
       Exp.case (Pat.constant (Const_int j)) (expr_of_constr pcd) in
     let cases = List.mapi make_case constrs in
     let case_count = Exp.constant (Const_int (List.length cases)) in
     [%expr fun rng ->
-	   [%e Exp.match_ [%expr random_case [%e case_count] rng]
-			  (cases @ [invalid_case])] ]
+           [%e Exp.match_ [%expr random_case [%e case_count] rng]
+                          (cases @ [invalid_case])] ]
   | Ptype_variant cs, _ ->
     let branch (w, pcd) cont =
       [%expr if w > [%e Exp.constant (Const_int w)]
-	     then [%e expr_of_constr pcd]
-	     else [%e cont] ] in
+             then [%e expr_of_constr pcd]
+             else [%e cont] ] in
     begin match cumulative pcd_attributes cs with
     | [] -> assert false
     | (w, pcd) :: cs ->
       [%expr fun rng -> let w = random_case_30b rng in
-			[%e List.fold branch cs (expr_of_constr pcd)] ]
+                        [%e List.fold branch cs (expr_of_constr pcd)] ]
     end
   | Ptype_record fields, _ ->
     let fields = fields |> List.map @@ fun pld ->
@@ -236,7 +236,7 @@ let str_of_type ~options ~path type_decl =
   let random_var =
     pvar (Ppx_deriving.mangle_type_decl (`Prefix "random") type_decl) in
   [Vb.mk (Pat.constraint_ random_var random_type)
-	 (Ppx_deriving.poly_fun_of_type_decl type_decl random_func)]
+         (Ppx_deriving.poly_fun_of_type_decl type_decl random_func)]
 
 let () =
   Ppx_deriving.register @@
@@ -244,7 +244,7 @@ let () =
     ~core_type: (fun typ -> [%expr fun rng -> [%e expr_of_typ typ]])
     ~type_decl_str: (fun ~options ~path type_decls ->
       [Str.value Recursive
-	(List.concat (List.map (str_of_type ~options ~path) type_decls))])
+        (List.concat (List.map (str_of_type ~options ~path) type_decls))])
     ~type_decl_sig: (fun ~options ~path type_decls ->
       List.concat (List.map (sig_of_type ~options ~path) type_decls))
     ()
